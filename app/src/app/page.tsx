413037fd76377717829f1e648aecafccbe173447
Jordy Slaats <Slaats.Jordy@icloud.com>
2025-09-30 13:24:15 +0200

    Added Docker

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Roosh",
+  description: "Welcome to the Roosh website",
+};
+
 export default function Home() {
   return (
     <h1>Hello world!</h1>
